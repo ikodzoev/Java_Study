0b6d46b3845abf84e5ce649561d2562ad647dafb
ikodzoev <loamaro@live.ru>
2023-02-27 12:47:15 +0500

    Edited Homework for seminar № 5

--- src/main/java/HW_5.java
+++ src/main/java/HW_5.java
@@ -7,6 +7,8 @@ import java.util.HashMap;
 
 public class HW_5 {
     public static void main(String[] args) {
+
+        // Создать множество, ключ и значение строки. Добавить шесть элементов. Вывести в консоль значения.
         HashMap<Integer, String> colorsFirst = new HashMap<>();
         colorsFirst.put(4, "белый");
         colorsFirst.put(8, "синий");
@@ -17,9 +19,18 @@ public class HW_5 {
         colorsFirst.forEach((k, v) -> System.out.println(k + " " + v));
         System.out.println(colorsFirst);
 
-        colorsFirst.forEach((k, v) -> System.out.println(k + " " + v + "!"));
+        // Добавить ко всем значениям символ "!".
+        colorsFirst.computeIfPresent(4, (k, v) -> v += '!');
+        colorsFirst.computeIfPresent(8, (k, v) -> v += '!');
+        colorsFirst.computeIfPresent(16, (k, v) -> v += '!');
+        colorsFirst.computeIfPresent(32, (k, v) -> v += '!');
+        colorsFirst.computeIfPresent(64, (k, v) -> v += '!');
+        colorsFirst.computeIfPresent(128, (k, v) -> v += '!');
+        System.out.println(colorsFirst);
+        colorsFirst.forEach((k, v) -> System.out.println(k + " " + v));
         System.out.println(colorsFirst);
 
+        // Создать второе множество с таким же обобщением. Ключи второго множества частично совпадают с ключами первого.
         HashMap<Integer, String> colorsSec = new HashMap<>();
         colorsFirst.put(2, "белый");
         colorsFirst.put(1, "синий");
@@ -28,6 +39,7 @@ public class HW_5 {
         colorsFirst.put(4, "красный");
         colorsFirst.put(6, "лиловый");
 
+        // Объединить значения во втором множестве и первом, если ключи совпадают. Вывести результат в консоль.
         for (Integer item : colorsFirst.keySet()) {
             colorsFirst.merge(item, colorsSec.getOrDefault(item, ""), (newVal, tempVal) -> newVal + tempVal);
         }
